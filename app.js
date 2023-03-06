a6adcc3ad36f6832128418a1b06eaf2e8d400366
santism98 <santiagosuarezmanso@gmail.com>
2023-03-06 13:03:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const colors=require('colors')
+
+
+const {arrayColores,arrayColores2}=require('./colores')
+
+console.log(arrayColores);
+console.log(arrayColores2);
---
+++ colores.js
@@ -0,0 +1,8 @@
+const arrayColores=['rojo', 'verde', 'azul'];
+const arrayColores2=['cian', 'magenta', 'amarillo'];
+module.exports={
+    arrayColores,
+    arrayColores2
+}
+
+
